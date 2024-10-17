96bbdd0e9ad1e10779a95c715554251814d12df2
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-18 00:39:12 +0200

    chore: simplify CI set-up Inline the dashboard.sh script (as its contents are now trivial), don't need a github token any more, and neither do I need to install jq.

--- dashboard.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Surface errors in this script to CI, so they get noticed.
-# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
-set -e -u -o pipefail
-
-python3 ./dashboard.py > ./index.html
